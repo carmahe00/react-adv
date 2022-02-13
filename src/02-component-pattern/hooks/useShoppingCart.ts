f08f1acee8fb07af0973a95b9cfdccbcd6412285
juan <juanpabloardilama97@gmail.com>
2022-02-13 14:24:52 -0500

    Control props alt

--- src/02-component-pattern/hooks/useShoppingCart.ts
+++ src/02-component-pattern/hooks/useShoppingCart.ts
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
-import { cart, onChangeArgs, ProductInCart } from '../interfaces/interfaces';
-const useShoppingCart = () =>{
+import { cart, onChangeArgs } from '../interfaces/interfaces';
+const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<cart>({})
 
     /**
@@ -11,7 +11,7 @@ const useShoppingCart = () =>{
     const onProductCountChange = ({ product, count }: onChangeArgs) => {
 
         setShoppingCart(oldShoppingCard => {
-            const productInCart: ProductInCart = oldShoppingCard[product.id] || { ...product, count: 0 }
+            /* const productInCart: ProductInCart = oldShoppingCard[product.id] || { ...product, count: 0 }
             if (Math.max(productInCart.count + count, 0) > 0) {
                 productInCart.count += count
                 return {
@@ -22,12 +22,20 @@ const useShoppingCart = () =>{
 
             //Borrar el producto
             delete oldShoppingCard[product.id] // => {}
-            return { ...oldShoppingCard }
+            return { ...oldShoppingCard } */
+            if (!count) {
+                delete oldShoppingCard[product.id] // => {}
+                return { ...oldShoppingCard }
+            }
+            return {
+                ...oldShoppingCard,
+                [product.id]: {...product, count}
+            }
         })
 
     }
     return {
-        
+
         onProductCountChange,
         shoppingCart
     }
